00c3d0af75ce6f18e92cfba4fe9059b142141da4
Jürmo Mihhailov <mihhailovjurmo@gmail.com>
2023-10-04 15:22:21 +0300

    Add error management to project

--- index.js
+++ index.js
@@ -10,18 +10,34 @@ app.set("views", path.join(__dirname, "views"))
 const parseUrl = require('body-parser');
 let encodeUrl = parseUrl.urlencoded({ extended: true });
 
-app.get('/', (req, res) => {
-	res.render('validate_form')
+let error = null
+app.get("/", (req, res) => {
+    res.render("page", {
+        data: null,
+        error: error,
+    })
 })
-
 
 const validId = require('./validate')
 
-app.post('/validate', encodeUrl, (req, res) => {
-	console.log('form data validation')
-	console.log(req.body)
-	console.log(req.body.id_code)
-	res.send(validId.idInfo(req.body.id_code))
+app.post('/', encodeUrl, (req, res) => {
+    let error = null
+    if (req.body.id_code === "") {
+        error = "Palun sisesta vormis andmed"
+    } else if(req.body.id_code.length != 11) {
+        error = "Palun sisesta korrektne isikukood"
+    }
+    if (error === null) {
+        res.render('page', {
+            data: validId.idInfo(req.body.id_code),
+            error: null,
+        })
+    } else {
+        res.render('page', {
+            data: null,
+            error: error,
+        })
+    }
 })
 
 app.listen(3000, () => {
